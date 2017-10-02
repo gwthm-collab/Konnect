07c3ea9a751ced225ab19b31f59a6ee336adbb9c
Gowthaman <gwthm11@gmail.com>
2017-10-02 14:26:16 +0530

    Initial Commit with text transfer

---
+++ ClientSide.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+
+public class ClientSide{
+    public static void main(String[] a){
+        String serName = a[0];
+        int port = Integer.parseInt(a[1]);
+        try{
+            System.out.println("Connecting to "+serName+" on port "+port);
+            Socket client = new Socket(serName, port);
+            System.out.println("Connected to " + client.getRemoteSocketAddress());
+            OutputStream ToServer = client.getOutputStream();
+            DataOutputStream out = new DataOutputStream(ToServer);
+            
+            out.writeUTF("Hello from " + client.getLocalSocketAddress());
+            InputStream FromServer = client.getInputStream();
+            DataInputStream in = new DataInputStream(FromServer);
+            
+            System.out.println("Server replies " + in.readUTF());
+            client.close();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+        }
+    }
+}
